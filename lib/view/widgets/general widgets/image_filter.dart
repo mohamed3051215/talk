03c348871f4eed3045c37a8e703193035d06b18c
Mohamed Ayman <mohamed3051215@gmail.com>
2022-08-11 21:16:40 +0200

    Adding ImageFilter widget to my general widgets Got this code from https://stackoverflow.com/a/64763333/18190914

---
+++ lib/view/widgets/general widgets/image_filter.dart
@@ -0,0 +1,37 @@
+// Got this code from https://stackoverflow.com/a/64763333/18190914
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import '../../../core/helpers/color_filter_gen.dart';
+
+class ImageFilter extends StatelessWidget {
+  double brightness, saturation, hue;
+  Widget child;
+  ImageFilter(
+      {required this.brightness,
+      required this.saturation,
+      required this.hue,
+      required this.child});
+  @override
+  Widget build(BuildContext context) {
+    return ColorFiltered(
+        colorFilter:
+            ColorFilter.matrix(ColorFilterGenerator().brightnessAdjustMatrix(
+          value: brightness,
+        )),
+        child: ColorFiltered(
+            colorFilter: ColorFilter.matrix(
+                ColorFilterGenerator().saturationAdjustMatrix(
+              value: saturation,
+            )),
+            child: ColorFiltered(
+              colorFilter:
+                  ColorFilter.matrix(ColorFilterGenerator.hueAdjustMatrix(
+                value: hue,
+              )),
+              child: child,
+            )));
+  }
+}
