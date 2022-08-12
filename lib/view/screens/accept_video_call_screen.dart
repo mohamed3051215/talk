45f776078977e5239eeeb2d7eee0807c44085230
Mohamed Ayman <mohamed3051215@gmail.com>
2022-08-12 21:38:29 +0200

    Adding AcceptVideoCallScreen screen to my app to handle video calls

---
+++ lib/view/screens/accept_video_call_screen.dart
@@ -0,0 +1,134 @@
+import 'dart:async';
+
+import 'package:assets_audio_player/assets_audio_player.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:chat_app/view/screens/video_call_screen.dart';
+import 'package:chat_app/view/screens/voice_call_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
+
+import '../../core/constants/colors.dart';
+import '../../core/models/user.dart';
+import '../widgets/general widgets/circle_button_for_call.dart';
+import '../widgets/general widgets/custom_text.dart';
+import '../widgets/general widgets/image_filter.dart';
+import '../widgets/general widgets/logo.dart';
+
+class AcceptVideoCallScreen extends StatefulWidget {
+  final UserModel userModel;
+  final String channelName, chatId, token;
+  final bool isBackground;
+  const AcceptVideoCallScreen(
+      {Key? key,
+      required this.userModel,
+      required this.channelName,
+      required this.chatId,
+      required this.token,
+      required this.isBackground})
+      : super(key: key);
+
+  @override
+  State<AcceptVideoCallScreen> createState() => _AcceptVideoCallScreenState();
+}
+
+class _AcceptVideoCallScreenState extends State<AcceptVideoCallScreen> {
+  AssetsAudioPlayer player = AssetsAudioPlayer.newPlayer();
+
+  @override
+  void initState() {
+    super.initState();
+    player.open(
+      Audio("assets/audios/ringtone1.wav"),
+      autoStart: true,
+    );
+
+    Timer.periodic(Duration(seconds: 30), (timer) {
+      if (mounted) {
+        player.stop();
+        timer.cancel();
+        Navigator.pop(context);
+      }
+      timer.cancel();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        Align(alignment: Alignment.topCenter, child: Logo()),
+        ImageFilter(
+          hue: 0,
+          brightness: -0.8,
+          saturation: 0.9,
+          child: Container(
+            width: Get.width,
+            height: Get.height,
+            child: CachedNetworkImage(
+              imageUrl: widget.userModel.image,
+              fit: BoxFit.cover,
+              placeholder: (context, url) => Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+          ),
+        ),
+        Align(
+            alignment: Alignment(0, -.5),
+            child: CustomText(
+                '${widget.userModel.username}\nIs Asking For Video Call ',
+                color: lightPurple,
+                fontSize: 35,
+                maxLines: 3,
+                textAlign: TextAlign.center,
+                fontWeight: FontWeight.w200)),
+        Align(
+            alignment: Alignment(0, .7),
+            child: Container(
+              width: 300,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  CircleButtonForCall(
+                    icon: Icons.phone,
+                    onPressed: () {
+                      if (widget.isBackground) {
+                        SystemNavigator.pop();
+                        SystemNavigator.pop();
+                        SystemNavigator.pop();
+                        player.stop();
+                        return;
+                      }
+                      Navigator.pop(context);
+                    },
+                    buttonColor: Colors.red,
+                    size: 80,
+                    iconColor: lightPurple,
+                    iconSize: 40,
+                  ),
+                  CircleButtonForCall(
+                    icon: Icons.phone,
+                    onPressed: () {
+                      Get.off(VideoCallScreen(
+                        channelName2: widget.channelName,
+                        token2: widget.token,
+                        chatId: widget.chatId,
+                        userModel: widget.userModel,
+                      ));
+                    },
+                    buttonColor: Colors.green,
+                    size: 80,
+                    iconColor: lightPurple,
+                    iconSize: 40,
+                  ),
+                ],
+              ),
+            ))
+      ],
+    ));
+  }
+}
