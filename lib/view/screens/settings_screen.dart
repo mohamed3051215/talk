5a2704ce3cac048ac66ca7c962fc5960a2bd623d
Mohamed Ayman <mohamed3051215@gmail.com>
2022-08-17 13:46:56 +0200

    Testing the storing functions into database

--- lib/view/screens/settings_screen.dart
+++ lib/view/screens/settings_screen.dart
@@ -1,15 +1,168 @@
+import 'dart:math';
+
+import 'package:chat_app/core/constants/fake_data.dart';
+import 'package:chat_app/core/controllers/home_screen_controller.dart';
+import 'package:chat_app/core/enums/message_type.dart';
+import 'package:chat_app/core/helpers/generate_random_string.dart';
+import 'package:chat_app/core/models/contact.dart';
+import 'package:chat_app/core/models/message.dart';
+import 'package:chat_app/core/service/message_storing_service.dart';
+import 'package:chat_app/view/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '../../core/constants/colors.dart';
+import '../widgets/general widgets/custom_text.dart';
 
 class SettingsScreen extends StatelessWidget {
-  const SettingsScreen({Key? key}) : super(key: key);
-
+  SettingsScreen({Key? key}) : super(key: key);
+  final MessageStoringService service = MessageStoringService();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: bgColor,
-
+      body: SingleChildScrollView(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(),
+              ElevatedButton(
+                  onPressed: () async {
+                    print(await service.getMessages("dsyogfysdgyu"));
+                  },
+                  child: CustomText(
+                    "get all messages",
+                    fontSize: 20,
+                  )),
+              ElevatedButton(
+                  onPressed: () async {
+                    await service.deleteDatabaseFile();
+                  },
+                  child: CustomText(
+                    "delete data base",
+                    fontSize: 20,
+                  )),
+              ElevatedButton(
+                  onPressed: () async {
+                    await service.init();
+                  },
+                  child: CustomText(
+                    "Init database",
+                    fontSize: 20,
+                  )),
+              ElevatedButton(
+                  onPressed: () {
+                    service.addMessage(Message(
+                        chatId: "dsyogfysdgyu",
+                        id: generateRandomString(10),
+                        type: MessageType.text,
+                        text: generateRandomString(50),
+                        date: DateTime.now(),
+                        from: mohamed.id,
+                        to: monika.id));
+                    service.addMessage(messages[0]);
+                  },
+                  child: CustomText(
+                    "Add message",
+                    fontSize: 20,
+                  )),
+              ElevatedButton(
+                  onPressed: () async {
+                    await service.addUser(mohamed);
+                  },
+                  child: CustomText(
+                    "add user",
+                    fontSize: 20,
+                  )),
+              ElevatedButton(
+                  onPressed: () async {
+                    print(await service.getUsers());
+                  },
+                  child: CustomText(
+                    "get all users",
+                    fontSize: 20,
+                  )),
+              ElevatedButton(
+                  onPressed: () async {
+                    print(await service.getMyUser());
+                  },
+                  child: CustomText(
+                    "get my user",
+                    fontSize: 20,
+                  )),
+              ElevatedButton(
+                  onPressed: () async {
+                    await service.addMyUser(mohamed);
+                  },
+                  child: CustomText(
+                    "add my user",
+                    fontSize: 20,
+                  )),
+              ElevatedButton(
+                  onPressed: () async {
+                    await service.addCall(calls[3]);
+                  },
+                  child: CustomText(
+                    "add call",
+                    fontSize: 20,
+                  )),
+              ElevatedButton(
+                  onPressed: () async {
+                    print(await service.getContacts());
+                  },
+                  child: CustomText(
+                    "get all contacts",
+                    fontSize: 20,
+                  )),
+              ElevatedButton(
+                  onPressed: () async {
+                    await service.addContact(
+                      ChatContact(
+                          chatId: "dsyogfysdgyu",
+                          messagesNotSeen: 2,
+                          contactUser: mohamed,
+                          lastMesssage: messages[0]),
+                    );
+                  },
+                  child: CustomText(
+                    "add contact",
+                    fontSize: 20,
+                  )),
+              ElevatedButton(
+                  onPressed: () async {
+                    print(await service.getLastMessage("dsyogfysdgyu"));
+                  },
+                  child: CustomText(
+                    "get last message",
+                    fontSize: 20,
+                  )),
+              ElevatedButton(
+                  onPressed: () {},
+                  child: CustomText(
+                    "get all messages",
+                    fontSize: 20,
+                  )),
+              ElevatedButton(
+                  onPressed: () {},
+                  child: CustomText(
+                    "get all messages",
+                    fontSize: 20,
+                  )),
+              ElevatedButton(
+                  onPressed: () {},
+                  child: CustomText(
+                    "get all messages",
+                    fontSize: 20,
+                  )),
+              ElevatedButton(
+                  onPressed: () {},
+                  child: CustomText(
+                    "get all messages",
+                    fontSize: 20,
+                  )),
+            ]),
+      ),
     );
   }
 }
